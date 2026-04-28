6fab012098e09414c9465b7889f6e1922957be67
masahiro120 <masahiro120@i.softbank.jp>
2026-04-29 01:03:14 +0900

    update

--- module.js
+++ module.js
@@ -1,3 +1,11 @@
 export function formatNumber(num) {
   return Number(num.toFixed(2)).toString();
 }
+
+export function tomonth(num) {
+  return Math.round(num / 12 * 10000);
+}
+
+export function formatYen(num) {
+  return new Intl.NumberFormat('ja-JP').format(num);
+}
